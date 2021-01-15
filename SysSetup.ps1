539600c41a56af357a1c3b240f780e4c8ed8bd98
Crocokyle <me@kyle-dixon.com>
2021-01-15 04:44:39 -1000

    Initial Commit

---
+++ SysSetup.ps1
@@ -0,0 +1,84 @@
+﻿$SearchDir = $env:LOCALAPPDATA + '\VALORANT\Saved\Config'
+$TokenizedResults = gci -Recurse -Filter "GameUserSettings.ini" -File -Path $SearchDir -Force
+$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+$SrcPath = $ScriptDir + '\GameUserSettingsSrc.ini'
+$TargetPath = $TokenizedResults.DirectoryName + '\GameUserSettingsSrc.ini'
+$ExistingSettings = $TokenizedResults.DirectoryName + '\GameUserSettings.ini'
+
+function WriteLauncher {
+    $fName = $ScriptDir + '\ValorantLauncher.bat'
+    New-Item $fName
+    'echo Patching Valorant screen resolution...' | Out-File $fName -Append -encoding "UTF8"
+    'copy ' + $TargetPath + ' ' + $TokenizedResults.DirectoryName + '\GameUserSettings.ini' | Out-File $fName -Append -encoding "UTF8"
+    'echo Killing your Windows taskbar until game has closed...' | Out-File $fName -Append -encoding "UTF8"
+    'echo Launching Valorant in Ultrawide' | Out-File $fName -Append -encoding "UTF8"
+    '"C:\Riot Games\Riot Client\RiotClientServices.exe" --launch-product=valorant --launch-patchline=live' | Out-File $fName -Append -encoding "UTF8"
+}
+
+function GetGraphicsInfo5 {
+    $existing_data = Get-Content $ExistingSettings
+    $firstFive = $existing_data | Select-Object -First 5    
+    return $firstFive
+}
+
+function GetGraphicsInfo15 {
+    $existing_data = Get-Content $ExistingSettings
+    $last = $existing_data | Select-Object -Last 15    
+    return $last
+}
+
+function WriteGameSettings {
+    $existingSettingsfile = Get-Content $ExistingSettings
+    Write-Host "Getting your native monitor resolution..."
+    $ScreenResolution = Get-WmiObject -Class Win32_DesktopMonitor | Select-Object ScreenWidth,ScreenHeight
+    Write-Host "Creating your Valorant graphics profile..."
+    $SrcIniName = $ScriptDir + '\GameUserSettingsSrc.ini'
+    New-Item $SrcIniName
+    GetGraphicsInfo5 | Out-File $SrcIniName -Append -encoding "UTF8"
+    'bShouldLetterbox=False' | Out-File $SrcIniName -Append -encoding "UTF8"
+    'bLastConfirmedShouldLetterbox=False' | Out-File $SrcIniName -Append -encoding "UTF8"
+    'bUseVSync=False' | Out-File $SrcIniName -Append -encoding "UTF8"
+    'bUseDynamicResolution=False' | Out-File $SrcIniName -Append -encoding "UTF8"
+    'ResolutionSizeX=' + $ScreenResolution.ScreenWidth | Out-File $SrcIniName -Append -encoding "UTF8"
+    'ResolutionSizeY=' + $ScreenResolution.ScreenHeight | Out-File $SrcIniName -Append -encoding "UTF8"
+    'LastUserConfirmedResolutionSizeX=' + $ScreenResolution.ScreenWidth | Out-File $SrcIniName -Append -encoding "UTF8"
+    'LastUserConfirmedResolutionSizeY=' + $ScreenResolution.ScreenHeight | Out-File $SrcIniName -Append -encoding "UTF8"
+    'WindowPosX=0' | Out-File $SrcIniName -Append -encoding "UTF8"
+    'WindowPosY=0' | Out-File $SrcIniName -Append -encoding "UTF8"
+    'LastConfirmedFullscreenMode=2' | Out-File $SrcIniName -Append -encoding "UTF8"
+    'PreferredFullscreenMode=2' | Out-File $SrcIniName -Append -encoding "UTF8"
+    'AudioQualityLevel=0' | Out-File $SrcIniName -Append -encoding "UTF8"
+    'LastConfirmedAudioQualityLevel=0' | Out-File $SrcIniName -Append -encoding "UTF8"
+    $existingSettingsfile | Select-Object -Index 19 | Out-File $SrcIniName -Append -encoding "UTF8"
+    'DesiredScreenWidth=' + $ScreenResolution.ScreenWidth | Out-File $SrcIniName -Append -encoding "UTF8"
+    'DesiredScreenHeight=' + $ScreenResolution.ScreenHeight | Out-File $SrcIniName -Append -encoding "UTF8"
+    'LastUserConfirmedDesiredScreenWidth=' + $ScreenResolution.ScreenWidth | Out-File $SrcIniName -Append -encoding "UTF8"
+    'LastUserConfirmedDesiredScreenHeight=' + $ScreenResolution.ScreenHeight | Out-File $SrcIniName -Append -encoding "UTF8"
+    $existingSettingsfile | Select-Object -Index 24 | Out-File $SrcIniName -Append -encoding "UTF8"
+    $existingSettingsfile | Select-Object -Index 25 | Out-File $SrcIniName -Append -encoding "UTF8"
+    $existingSettingsfile | Select-Object -Index 26 | Out-File $SrcIniName -Append -encoding "UTF8"
+    $existingSettingsfile | Select-Object -Index 27 | Out-File $SrcIniName -Append -encoding "UTF8"
+    $existingSettingsfile | Select-Object -Index 28 | Out-File $SrcIniName -Append -encoding "UTF8"
+    $existingSettingsfile | Select-Object -Index 29 | Out-File $SrcIniName -Append -encoding "UTF8"
+    $existingSettingsfile | Select-Object -Index 30 | Out-File $SrcIniName -Append -encoding "UTF8"
+    'FullscreenMode=2' | Out-File $SrcIniName -Append -encoding "UTF8"
+    '' | Out-File $SrcIniName -Append -encoding "UTF8"
+    '[/Script/Engine.GameUserSettings]' | Out-File $SrcIniName -Append -encoding "UTF8"
+    'bUseDesiredScreenHeight=True' | Out-File $SrcIniName -Append -encoding "UTF8"
+    GetGraphicsInfo15 | Out-File $SrcIniName -Append -encoding "UTF8"
+}
+
+$host.ui.RawUI.WindowTitle = "Kyle's Valorant Ultrawide Patch Installer"
+Write-Host ""
+Write-Host "Kyle's Valorant Ultrawide Patch Installer"
+Write-Host " "
+Write-Host "Elevating permissions..."
+Write-Host "Getting system information..."
+
+WriteGameSettings
+Write-Host ""
+Write-Host "Copying patch files to game directory..."
+Write-Host ""
+copy $SrcPath $TargetPath
+Write-Host "Creating your custom Valorant launch script..."
+WriteLauncher
---
+++ install.bat
@@ -0,0 +1,31 @@
+@echo off
+mode con: cols=220 lines=50
+color 09
+title Kyle's Valorant Ultrawide Patch Installer
+echo Kyle's Valorant Ultrawide Patch Installer
+echo .
+echo .
+echo Launching the big guns! - Getting your system information, graphics settings, and creating your custom Valorant profile...
+Powershell.exe -executionpolicy remotesigned -File SysSetup.ps1
+echo Creating install directory
+mkdir "%HOMEPATH%\Documents\ValorantUltrawideHack"
+echo Moving patch files to install directory...
+copy ValorantLauncher.bat "%HOMEPATH%\Documents\ValorantUltrawideHack\ValorantLauncher.bat
+copy GameUserSettingsSrc.ini "%HOMEPATH%\Documents\ValorantUltrawideHack\GameUserSettingsSrc.ini"
+echo Creating desktop shortcut...
+copy "Valorant Ultrawide Launcher.lnk" "%HOMEPATH%\Desktop\Valorant Ultrawide Launcher.lnk"
+echo Cleaning up install files...
+del GameUserSettingsSrc.ini
+del ValorantLauncher.bat
+del "Valorant Ultrawide Launcher.lnk"
+del SysSetup.ps1
+echo .
+echo ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+echo Complete! Please check the log for errors above.
+echo .
+echo Please be patient. The splash screen will appear stretched because the game was not written for ultrawide. The game itself and all menus will be in true ultrawide, NOT stretched. 
+echo .
+echo To launch Valorant in Ultrawide resolution, please click the "Valorant Ultrawide Launcher" shortcut on your Desktop. 
+echo ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+pause
+del install.bat
